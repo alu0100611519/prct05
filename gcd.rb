54782ccfb1c8b900117dc18f145a6a645a78ed02
Norberto Garcia Gaspar <alu0100611519@ull.edu.es>
2013-10-18 15:24:17 +0100

    ultima actualizacion

--- gcd.rb
+++ gcd.rb
@@ -1,4 +1,9 @@
-def gcd(u, v)
+#! /usr/local/rvm/rubies/ruby-2.0.0-p247/bin/ruby
+
+
+
+
+  def gcd(u, v)
   u, v = u.abs, v.abs
   while v != 0 # Este era el error, no puede ser v == 0 sino mientras se distinto de 0 (!=0)
     u, v = v, u % v
--- racional.rb
+++ racional.rb
@@ -1,106 +1,142 @@
-# Implementar en este fichero la clase para crear objetos racionales
+#! /usr/local/rvm/rubies/ruby-2.0.0-p247/bin/ruby
+
 
 require "./gcd.rb"
 
+
+
+
+
+
 class Fraccion
-  attr_reader :num, :den
+	      attr_reader :num, :den
   
   
-  #funcion para inicializar los numeros de la fraccion
-  def initialize(num, den)
+	    #funcion para inicializar los numeros de la fraccion
+  
+	      
+			def initialize(num, den)
     
     
-	raise ArgumentError, 'Argumento no enteros.' unless num.is_a? Interger and den.is_a? Integer
-	raise ArgumentError, 'Denominador nulo.' unless dem != 0
+			raise ArgumentError, 'Argumento no enteros.' unless num.is_a? Interger and den.is_a? Integer
+			raise ArgumentError, 'Denominador nulo.' unless dem != 0
 	
 	
-	  #creando variables de istancias para modificar los numeros leidos.
-		 @num = num 
-		 @den = den
+			#creando variables de istancias para modificar los numeros leidos.
+			  @num = num 
+			  @den = den
 		
 		 
 		 
-	#llamamos a la funcion
-		 minima
+			#llamamos a la funcion
+			minima
     
-  end 
+			end
+		  
+		  
+		  
   
-  #funcion para reducir la fraccion a la minima expresion.
-  def minima
+			 #funcion para reducir la fraccion a la minima expresion.
+			 def minima
 
-	 div = gcd(num, den) #llamamos a la funcion gcd para sacar el comun divisor de los numeros, creamos una variable y la igualamos para poder reducir la fraccion
-	 @num = @num/div
-	 @den = @den/div
+			 div = gcd(num, den) #llamamos a la funcion gcd para sacar el comun divisor de los numeros, creamos una variable y la igualamos para poder reducir la fraccion
+			 @num = @num/div
+			 @den = @den/div
 	 
   
   
-  end
-  #funcion to_s, devuelve la representacion de la cadena es decir la fraccion.
-  def to_s
+			 end
+			 
+			 
+			 
+			#funcion to_s, devuelve la representacion de la cadena es decir la fraccion.
+			def to_s
     
-	  #fraccion 
-	    print "@{num}/@{den}"
-  end 
+			#fraccion 
+			 "@{num}/@{den}"
+			end 
   
   
-  #funcion para resolver una suma de fracciones, el metodo puede hacer una suma de la fraccion tanto con una fraccion como con un entero
-  def suma(ra)
-    
-    
-    #mensaje de error, el raise lo que evita junto con unless es que si NO se cumple salta el mensaje de error y no se realiza el metodo.
-	raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
+  
+			
+			
+			#funcion para resolver una suma de fracciones, el metodo puede hacer una suma de la fraccion tanto con una fraccion como con un entero
+  
+			def suma(ra)
+			#mensaje de error, el raise lo que evita junto con unless es que si NO se cumple salta el mensaje de error y no se realiza el metodo.
+	
+			  raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
+			  #la funcion crea un objeto de la clase Fraccion con las caracteristicas obbtenidas, dependiendo si lo que se va a sumar es un entero o otra Fraccion se hara una cosa o otra.
+			  if racional.is_a? Integer
+			    r = Fraccion.new(@num + @dem*ra, @den) 
+			  else
+			    r = Fraccion.new(@num*ra.den + @den*ra.num, @den*ra.den)
+			  end
+			end
+  
+  
+  
+			#funcion resta
+  
+  
+			def resta(ra)
     
 	
-	#la funcion crea un objeto de la clase Fraccion con las caracteristicas obbtenidas, dependiendo si lo que se va a sumar es un entero o otra Fraccion se hara una cosa o otra.
-	if racional.is_a? Integer
-	r = Fraccion.new(@num + @dem*ra, @den) 
-	else
-	r = Fraccion.new(@num*ra.den + @den*ra.num, @den*ra.den)
-	end
+			  raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
 	
 	
-  end
-  
-  
-  #funcion resta
-  
-  def resta(ra)
-    
-	raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
+			  if racional.is_a? Integer
 	
-	if racional.is_a? Integer
-	r = Fraccion.new(@num - @dem*ra, @den) 
-	else
-	r = Fraccion.new(@num*ra.den - @den*ra.num, @den*ra.den)
-	end
+			    r = Fraccion.new(@num - @dem*ra, @den) 
 	
-  end
+			  else
+	
+			    r = Fraccion.new(@num*ra.den - @den*ra.num, @den*ra.den)
+	
+			  end
+	
+ 
+			end
   
-  #funcion producto
-  def producto(ra)
-    raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
-    
-    if racional.is_a? Integer
-	r = Fraccion.new(@num * @dem*ra, @den) 
-	else
-	r = Fraccion.new(@num*ra.num , @den*ra.den)
-	end
-  end
   
-  #funcion division
-  def division(ra)
-    raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
-    
-    
-    if racional.is_a? Integer
-	r = Fraccion.new(@num , @den * ra) 
-	else
-	r = Fraccion.new(@num*ra.den, @den*ra.num)
-	end
-  end
-    
-    
-    
-    
+			#funcion producto
   
+			def producto(ra)
+    
+			  raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
+    
+    
+			  if racional.is_a? Integer
+	
+			    r = Fraccion.new(@num * @dem*ra, @den) 
+	  
+			  else
+	
+			    r = Fraccion.new(@num*ra.num , @den*ra.den)
+	
+			  end
+  
+			end
+  
+  
+			#funcion division
+  
+			def division(ra)
+    
+			  raise ArgumentError, 'Argumento no admitido', unless ra.is_a? Fraccion or ra.is_a? Interger
+    
+    
+    
+			  if racional.is_a? Integer
+	
+			    r = Fraccion.new(@num , @den * ra) 
+	
+			  else
+	
+			    r = Fraccion.new(@num*ra.den, @den*ra.num)
+	
+			  end
+  
+			end
+
 end
